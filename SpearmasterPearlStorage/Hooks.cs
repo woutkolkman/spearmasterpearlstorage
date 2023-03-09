27c2a21783cf4c79cf07d42aa2cc91311cc10df6
woutkolkman <wout.kolkman1@hotmail.com>
2023-03-09 19:47:11 +0100

    spearmaster eat

--- SpearmasterPearlStorage/Hooks.cs
+++ SpearmasterPearlStorage/Hooks.cs
@@ -7,20 +7,13 @@ namespace SpearmasterPearlStorage
 {
     class Hooks
     {
-        static BindingFlags propFlags = BindingFlags.Instance | BindingFlags.Public;
-        static BindingFlags myMethodFlags = BindingFlags.Static | BindingFlags.Public;
-
-
         public static void Apply()
         {
-            //selects room to place GameController type
-            //On.Room.Loaded += RoomLoadedHook;
+            On.Player.GrabUpdate += PlayerGrabUpdateHook;
 
-            //Moon OracleGetToPos RuntimeDetour
-            /*Hook SLOracleBehaviorHasMarkOracleGetToPosHook = new Hook(
-                typeof(SLOracleBehaviorHasMark).GetProperty("OracleGetToPos", propFlags).GetGetMethod(),
-                typeof(Hooks).GetMethod("SLOracleBehaviorHasMark_OracleGetToPos_get", myMethodFlags)
-            );*/
+            On.Player.Regurgitate += PlayerRegurgitateHook;
+
+            On.Player.CanBeSwallowed += PlayerCanBeSwallowedHook;
         }
 
 
@@ -30,10 +23,59 @@ namespace SpearmasterPearlStorage
         }
 
 
-        //creates GameController object
-        /*static void AbstractPhysicalObjectRealizeHook(On.AbstractPhysicalObject.orig_Realize orig, AbstractPhysicalObject self)
+        //allows spearmaster to eat/put items in stomach
+        static void PlayerGrabUpdateHook(On.Player.orig_GrabUpdate orig, Player self, bool eu)
         {
+            bool isSpearmaster = (self.SlugCatClass == MoreSlugcats.MoreSlugcatsEnums.SlugcatStatsName.Spear);
+            bool hasFreeHand = self.FreeHand() != -1;
+
+            //temporary set to Survivor
+            if (isSpearmaster && !hasFreeHand)
+                self.SlugCatClass = SlugcatStats.Name.White;
+
+            orig(self, eu);
+
+            //reset to Spearmaster
+            if (isSpearmaster)
+                self.SlugCatClass = MoreSlugcats.MoreSlugcatsEnums.SlugcatStatsName.Spear;
+        }
+
+
+        //regurgitate the correct object
+        static void PlayerRegurgitateHook(On.Player.orig_Regurgitate orig, Player self)
+        {
+            //by default spearmaster has nothing in stomach, and creates a new SpearMasterPearl when regurgitating
+            //this hook allows objectInStomach to be regurgitated
+            bool isSpearmaster = (self.SlugCatClass == MoreSlugcats.MoreSlugcatsEnums.SlugcatStatsName.Spear);
+
+            //temporary set to Survivor
+            if (isSpearmaster && self.objectInStomach != null)
+                self.SlugCatClass = SlugcatStats.Name.White;
+
             orig(self);
-        }*/
+
+            //reset to Spearmaster
+            if (isSpearmaster)
+                self.SlugCatClass = MoreSlugcats.MoreSlugcatsEnums.SlugcatStatsName.Spear;
+        }
+
+
+        //changes return value CanBeSwallowed for Spearmaster to TRUE
+        static bool PlayerCanBeSwallowedHook(On.Player.orig_CanBeSwallowed orig, Player self, PhysicalObject testObj)
+        {
+            bool isSpearmaster = (self.SlugCatClass == MoreSlugcats.MoreSlugcatsEnums.SlugcatStatsName.Spear);
+
+            //temporary set to Survivor
+            if (isSpearmaster)
+                self.SlugCatClass = SlugcatStats.Name.White;
+
+            bool ret = orig(self, testObj);
+
+            //reset to Spearmaster
+            if (isSpearmaster)
+                self.SlugCatClass = MoreSlugcats.MoreSlugcatsEnums.SlugcatStatsName.Spear;
+
+            return ret;
+        }
     }
 }
