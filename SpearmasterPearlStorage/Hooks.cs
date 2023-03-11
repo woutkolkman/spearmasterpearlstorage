43ae6dc79f6d881696f100e366bd629f8ac607e3
woutkolkman <wout.kolkman1@hotmail.com>
2023-03-11 22:15:15 +0100

    overwrite part of spearmaster swallow animation

--- SpearmasterPearlStorage/Hooks.cs
+++ SpearmasterPearlStorage/Hooks.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using UnityEngine;
+using RWCustom;
 using System.Reflection;
 using MonoMod.Cil;
 using Mono.Cecil.Cil;
@@ -17,6 +18,9 @@ namespace SpearmasterPearlStorage
 
             //makes Spearmaster be able to only swallow pearls
             On.Player.CanBeSwallowed += PlayerCanBeSwallowedHook;
+
+            //overwrite Spearmaster swallow animation
+            On.PlayerGraphics.Update += PlayerGraphicsUpdateHook;
         }
 
 
@@ -42,6 +46,10 @@ namespace SpearmasterPearlStorage
 
             orig(self);
 
+            //offset position for regurgitated object (in front of hole)
+            if (isSpearmaster && !self.Stunned && self.standing && temp.realizedObject?.firstChunk != null)
+                temp.realizedObject.firstChunk.pos += new Vector2(0,-10);
+
             //restore objectInStomach if pearl is extracted by Pebbles
             if (isSpearmaster && self.Stunned)
                 self.objectInStomach = temp;
@@ -58,6 +66,44 @@ namespace SpearmasterPearlStorage
             if (self.SlugCatClass == MoreSlugcats.MoreSlugcatsEnums.SlugcatStatsName.Spear && testObj is DataPearl)
                 return true;
             return orig(self, testObj);
+        }
+
+
+        //overwrite Spearmaster swallow animation
+        static void PlayerGraphicsUpdateHook(On.PlayerGraphics.orig_Update orig, PlayerGraphics self)
+        {
+            orig(self);
+
+            bool isSpearmaster = (self.player.SlugCatClass == MoreSlugcats.MoreSlugcatsEnums.SlugcatStatsName.Spear);
+
+            //hands
+            if (isSpearmaster && 
+                self.player.swallowAndRegurgitateCounter > 10 && 
+                self.player.objectInStomach == null)
+            {
+                for (int i = 0; i < self.player.graphicsModule.bodyParts.Length; i++)
+                {
+                    if (!(self.player.graphicsModule.bodyParts[i] is SlugcatHand))
+                        continue;
+                    SlugcatHand hand = self.player.graphicsModule.bodyParts[i] as SlugcatHand;
+
+                    //check which limb will swallow
+                    int limbToSwallow = -1;
+                    int j = 0;
+                    while (limbToSwallow < 0 && j < 2) {
+                        if (self.player.grasps[j] != null && self.player.CanBeSwallowed(self.player.grasps[j].grabbed))
+                            limbToSwallow = j;
+                        j++;
+                    }
+
+                    //offset hands downwards -10f (originally -4f)
+                    if (limbToSwallow == hand.limbNumber) {
+                        float progress = Mathf.InverseLerp(10f, 90f, (float)self.player.swallowAndRegurgitateCounter);
+                        if (progress > 0.5f)
+                            hand.relativeHuntPos = new Vector2(0f, -10f) + Custom.RNV() * 2f * UnityEngine.Random.value * Mathf.InverseLerp(0.5f, 1f, progress);
+                    }
+                }
+            }
         }
     }
 }
